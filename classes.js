ccb6b361ef7eec9816ed062536f3f7311cf41c63
bhnash74 <bhn774@gmail.com>
2021-09-02 21:48:21 -0500

    Added classes, started methods

--- classes.js
+++ classes.js
@@ -1,2 +1,26 @@
 "use strict"
 const prompt = require('prompt-sync')();
+
+class Human extends Player {
+
+    constructor(rock, paper, scissors, lizard, Spock) {
+        super(1,2,3,4,5);
+        this.roll = null;
+    }
+    takesTurn() {
+        parseInt(prompt(`Make your choice:\n1) Rock\n2)Paper\n3) Scissors\n4 Lizard\5) Spock`));
+           return roll;
+       }
+}
+
+class AI extends Player {
+
+    constructor(rock, paper, scissors, lizard, Spock) {
+        super(1,2,3,4,5);
+        this.roll = null;
+    }
+    takesTurn() {
+       roll= (Math.floor(Math.random()*5));
+           return roll;
+       }
+}
--- player.js
+++ player.js
@@ -1,2 +1,20 @@
 "use strict"
 const prompt = require('prompt-sync')();
+class Player {
+    constructor(rock, paper, scissors, lizard, Spock) {
+        this.rock = rock;
+        this.paper = paper;
+        this.scissors = scissors;
+        this.lizard = lizard;
+        this.Spock = Spock;
+        this.roll = null;
+    }
+
+    takesTurn() {
+        parseInt(prompt(`Make your choice:\n1) Rock\n2)Paper\n3) Scissors\n4 Lizard\5) Spock`));
+           return roll;
+       }
+       
+
+}
+ 
