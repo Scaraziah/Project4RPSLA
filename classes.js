7f8490ece0d0966b61cbb01761b046ff2e547881
Larry Hatcer <actswolf@gmail.com>
2021-09-07 21:43:33 -0500

    Had to wipe and start over. Player page set up and everything else is wiped.

--- classes.js
+++ classes.js
@@ -1,26 +1,3 @@
 "use strict"
 const prompt = require('prompt-sync')();
 
-class Human extends Player {
-
-    constructor(rock, paper, scissors, lizard, Spock) {
-        super(1,2,3,4,5);
-        this.roll = null;
-    }
-    takesTurn() {
-        parseInt(prompt(`Make your choice:\n1) Rock\n2)Paper\n3) Scissors\n4 Lizard\n5) Spock`));
-           return roll;
-       }
-}
-
-class AI extends Player {
-
-    constructor(rock, paper, scissors, lizard, Spock) {
-        super(1,2,3,4,5);
-        this.roll = null;
-    }
-    takesTurn() {
-       roll= (Math.floor(Math.random()*5));
-           return roll;
-       }
-}
--- player.js
+++ player.js
@@ -3,49 +3,14 @@ const prompt = require('prompt-sync')();
 
 class Player {
     constructor(name) {
-       this.name= name;
-       this.w=0;
-       this.l=0;
-       this.t=0;
-       this.roll=0;
-    
+       this.name = name;
+       this.win = 0;
+       this.gestures = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+       this.selectGest = null;
     }
-   // takesTurn() {
-   //     var roll = parseInt(prompt(this.name+`: Make your choice:\n1) Rock\n2)Paper\n3) Scissors\n4 Lizard\n5) Spock\n`));
-   //    return roll;
-   // }
-       
-}
-
-class Human extends Player {
-
-    constructor(name) {
-        super(name);
-       
-    }
-    takesTurn() {
-        var roll = parseInt(prompt(this.name+`: Make your choice:\n1) Rock\n2)Paper\n3) Scissors\n4 Lizard\n5) Spock\n`));
-        console.log (this.name+' your roll is:'+ roll);
-        return this.roll=roll;
-    }
-    winningGesture(){ 'That is GREAT!!';
+    chosenGest(){
+        //put in code by player type
     }
 }
 
-class AI extends Player {
-
-    constructor(name) {
-        super(name);
-    }
-   takesTurn() {
-        var roll= (Math.floor(Math.random()*5));
-        console.log (this.name+' your roll is:'+ roll);
-        return this.roll=roll;
-    }
-    winningGesture(){
-        '01000001 01010111 01000101 01010011 01001111 01001101 01000101 00100001 00100001 00001101 00001010';
-
-    }
-} 
-module.exports =  AI;Human; Player.roll;
-//hi==
+module.export = Player;
